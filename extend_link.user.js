e7878799638ccc8f571823833e5d316f66934a58
Jormund <etiennebotek@gmail.com>
2016-10-13 20:45:53 +0200

    Handle https://ingress.com/intel

--- extend_link.user.js
+++ extend_link.user.js
@@ -2,18 +2,16 @@
 // @id             iitc-plugin-extend-link@Jormund
 // @name           IITC plugin: extend link
 // @category       Layer
-// @version        0.1.3.20160822.1616
+// @version        0.1.3.20161013.2028
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @downloadURL    https://raw.githubusercontent.com/Jormund/extend_link/master/extend_link.user.js
-// @description    [2016-08-22-1616] Draw the line between consecutive bookmarks and extend it
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
+// @description    [2016-10-13-2028] Draw the line between consecutive bookmarks and extend it
+// @include        https://ingress.com/intel*
+// @include        http://ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
 // @grant          none 
 // ==/UserScript==
 
