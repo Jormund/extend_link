b83745a8465f1c9acfac09d7d797a7ebd6f37198
Jormund <etiennebotek@gmail.com>
2018-01-24 20:06:37 +0100

    cdn.rawgit url

---
+++ extend_link.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @id             iitc-plugin-extend-link@Jormund
+// @name           IITC plugin: extend link
+// @category       Layer
+// @version        0.1.3.20180124.2001
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://cdn.rawgit.com/Jormund/extend_link/master/extend_link.meta.js
+// @downloadURL    https://cdn.rawgit.com/Jormund/extend_link/master/extend_link.user.js
+// @description    [2016-10-13-2028] Draw the line between consecutive bookmarks and extend it
+// @include        https://ingress.com/intel*
+// @include        http://ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @grant          none 
+// ==/UserScript==
